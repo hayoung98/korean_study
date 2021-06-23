19809874c07a06569c1ad2e90ec402f4efc6e81b
hayoung98 <jaskk870506@gmail.com>
2021-06-24 00:28:55 +0800

    first upload

---
+++ header.js
@@ -0,0 +1,35 @@
+header='<nav class="navbar navbar-default navbar-expand-lg" role="navigation">\
+            <a class="navbar-brand animate__animated animate__rollIn" href="./index.html">KOREAN STUDY</a>\
+            <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbar-collapse-01"></button>\
+            <div class="collapse navbar-collapse" id="navbar-collapse-01">\
+                <ul class="nav navbar-nav mr-auto">\
+                    <li class="dropdown hh">\
+                    <a class="animate__animated animate__bounceIn" href="./character.html">韓文字母</a>\
+                    <ul class="dropdown-menu mm animate__animated animate__fadeIn">\
+                        <li class="dropdown-list"><a href="./character.html#constuction">基本架構</a></li>\
+                        <li class="dropdown-list"><a href="./character.html#constuction1">&emsp;&emsp;韓文字的基本組合</a></li>\
+                        <li class="dropdown-list"><a href="./character.html#constuction2">&emsp;&emsp;韓文字的書寫筆順</a></li>\
+                        <li class="dropdown-list"><a href="./character.html#introduction">韓文字母</a></li>\
+                        <li class="dropdown-list"><a href="./character.html#introduction1">&emsp;&emsp;單母音</a></li>\
+                        <li class="dropdown-list"><a href="./character.html#introduction2">&emsp;&emsp;複合母音</a></li>\
+                        <li class="dropdown-list"><a href="./character.html#introduction3">&emsp;&emsp;單子音</a></li>\
+                        <li class="dropdown-list"><a href="./character.html#introduction4">&emsp;&emsp;雙子音</a></li>\
+                        <li class="dropdown-list"><a href="./character.html#all">&emsp;&emsp;拼音總表</a></li>\
+                    </ul>\
+                    </li>\
+                    <li class="dropdown hh">\
+                    <a class="animate__animated animate__bounceIn" href="./number.html">韓文數字</a>\
+                    <ul class="dropdown-menu mm animate__animated animate__fadeIn">\
+                        <li class="dropdown-list"><a href="./number.html#number">漢字數字、固有數字</a></li>\
+                        <li class="dropdown-list"><a href="./number.html#number1">&emsp;&emsp;比較</a></li>\
+                        <li class="dropdown-list"><a href="./number.html#number2">&emsp;&emsp;漢字數字</a></li>\
+                        <li class="dropdown-list"><a href="./number.html#number3">&emsp;&emsp;固有數字</a></li>\
+                        <li class="dropdown-list"><a href="./number.html#number4">&emsp;&emsp;常見的韓文數字量詞</a></li>\
+                    </ul>\
+                    </li>\
+                    <li><a class="animate__animated animate__bounceIn" href="./vocabulary.html">常用單字</a></li>\
+                    <li><a class="animate__animated animate__bounceIn" href="./sentence.html">常用句子</a></li>\
+                </ul></div>\
+        </nav>';
+
+document.write(header);
